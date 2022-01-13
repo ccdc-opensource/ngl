1bd4551adade8b3931f8efa7ff483e0c1f970165
Andy Widdess <awiddess@ccdc.cam.ac.uk>
2022-01-13 15:57:56 +0000

    VIS-64-a Added behavious for generating unitcell with matrix

--- src/parser/ccdcmol2-parser.ts
+++ src/parser/ccdcmol2-parser.ts
@@ -1,6 +1,6 @@
 /**
- * @file Mol2 Parser
- * @author Alexander Rose <alexander.rose@weirdbyte.de>
+ * @file CCDCMol2 Parser
+ * @author CCDC
  * @private
  */
 
@@ -11,7 +11,12 @@ import {
   calculateBondsBetween, calculateBondsWithin
 } from '../structure/structure-utils'
 import StructureParser from './structure-parser'
+import { Vector3, Matrix4 } from 'three'
+import Assembly from '../symmetry/assembly'
+import Structure from '../structure/structure'
+import Unitcell, { UnitcellParams } from '../symmetry/unitcell'
 
+const reInteger = /^[1-9]$/
 const reWhitespace = /\s+/
 const bondTypes: {[k: string]: number} = {
   '1': 1,
@@ -67,6 +72,19 @@ class CCDCMol2Parser extends StructureParser {
     let moleculeRecordType = 1
     let atomRecordType = 2
     let bondRecordType = 3
+    let crysinRecordType = 4
+
+    const unitcellDict: Partial<{
+      origx: Matrix4
+      scale: Matrix4
+      a: number
+      b: number
+      c: number
+      alpha: number
+      beta: number
+      gamma: number
+      spacegroup: string
+    }> = {}
 
     const ap1 = s.getAtomProxy()
     const ap2 = s.getAtomProxy()
@@ -96,6 +114,8 @@ class CCDCMol2Parser extends StructureParser {
             }
           } else if (line === '@<TRIPOS>BOND') {
             currentRecordType = bondRecordType
+          } else if (line === '@<TRIPOS>CRYSIN') {
+              currentRecordType = crysinRecordType
           } else {
             currentRecordType = 0
           }
@@ -183,20 +203,48 @@ class CCDCMol2Parser extends StructureParser {
           const order = bondTypes[ ls[ 3 ] ]
 
           s.bondStore.addBond(ap1, ap2, order)
+        }  else if (currentRecordType === crysinRecordType) {
+          const ls = line.split(reWhitespace);
+  
+          const aLength = parseFloat(ls[0]);
+          const bLength = parseFloat(ls[1]);
+          const cLength = parseFloat(ls[2]);
+         
+          const alpha = parseFloat(ls[3]);
+          const beta = parseFloat(ls[4]);
+          const gamma = parseFloat(ls[5]);
+  
+          unitcellDict.a = aLength;
+          unitcellDict.b = bLength;
+          unitcellDict.c = cLength;
+          unitcellDict.alpha = alpha;
+          unitcellDict.beta = beta;
+          unitcellDict.gamma = gamma;
         }
       }
     }
 
     const lines = jsonData.mol2.split('\n');
     _parseChunkOfLines(0, lines.length, lines)
-    
+
+    if (unitcellDict.a !== undefined) { 
+      s.unitcell = new Unitcell(unitcellDict as UnitcellParams) 
+    } else { 
+      s.unitcell = undefined 
+    } 
+
+    const operators:string[][] = [];
     const spacegroupOperators = (spacegroupOperators: string) =>{
             // x,y,z;-x,-y,1/2+z;1/2-x,1/2+y,1/2+z;1/2+x,1/2-y,z
       spacegroupOperators.split(";").forEach(op => {
-        // op.split(",").forEach(o => {
-        
-        // });
+        const semop:string[] = [];
+        op.split(",").forEach(o => {
+          semop.push(o.toUpperCase());
+        });
+        operators.push(semop);
       });
+
+      this._buildUnitcellAssembly(s, operators);
     }
 
     sb.finalize()
@@ -207,10 +255,178 @@ class CCDCMol2Parser extends StructureParser {
     s.finalizeBonds()
     assignResidueTypeBonds(s)
     calculateSecondaryStructure(s)
-
     spacegroupOperators(jsonData.spacegroupOperators);
 
     if (Debug) Log.timeEnd('Mol2Parser._parse ' + this.name)
+  }
+
+  _setSymmetryOperations (operators: string[][]) {
+
+    const matrixDict: { [k: string]: Matrix4 } = {}
+  
+    if (operators === undefined) {
+      console.warn(`spacegroup '${operators}' not found`)
+      return matrixDict
+    }
+  
+    operators.forEach(function (symop) {
+      let row = 0
+      const matrix = new Matrix4().set(
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 1
+      )
+      const me = matrix.elements
+  
+      matrixDict[ symop.toString() ] = matrix
+  
+      symop.forEach(function (elm) {
+        let negate = false
+        let denominator = false
+  
+        for (let i = 0, n = elm.length; i < n; ++i) {
+          const c = elm[ i ]
+  
+          if (c === '-') {
+            negate = true
+          } else if (c === '+') {
+            negate = false
+          } else if (c === '/') {
+            denominator = true
+          } else if (c === 'X') {
+            me[ 0 + row ] = negate ? -1 : 1
+          } else if (c === 'Y') {
+            me[ 4 + row ] = negate ? -1 : 1
+          } else if (c === 'Z') {
+            me[ 8 + row ] = negate ? -1 : 1
+          } else if (reInteger.test(c)) {
+            const integer = parseInt(c)
+            if (denominator) {
+              me[ 12 + row ] /= integer
+            } else {
+              me[ 12 + row ] = integer
+            }
+          } else {
+            Log.warn(`getSymmetryOperations: unknown token '${c}'`)
+          }
+        }
+  
+        row += 1
+      })
+    })
+  
+    return matrixDict
+  }
+
+  _buildUnitcellAssembly (structure: Structure, operators: string[][]) {
+    if (!structure.unitcell) return
+  
+    if (Debug) Log.time('buildUnitcellAssembly')
+  
+    const uc = structure.unitcell
+  
+    const structureCenterFrac = structure.center.clone().applyMatrix4(uc.cartToFrac)
+    const centerFrac = structureCenterFrac.clone().floor()
+    const symopDict: { [K: string]: Matrix4 } = this._setSymmetryOperations(operators)
+  
+    const centerFracSymop = new Vector3()
+    const positionFracSymop = new Vector3()
+  
+    function getMatrixList (shift?: Vector3) {
+      const matrixList: Matrix4[] = []
+  
+      Object.keys(symopDict).forEach(function (name) {
+        const m = symopDict[ name ].clone()
+  
+        centerFracSymop.copy(structureCenterFrac).applyMatrix4(m).floor()
+        positionFracSymop.setFromMatrixPosition(m)
+        positionFracSymop.sub(centerFracSymop)
+        positionFracSymop.add(centerFrac)
+  
+        if (shift) positionFracSymop.add(shift)
+  
+        m.setPosition(positionFracSymop)
+        m.multiplyMatrices(uc.fracToCart, m)
+        m.multiply(uc.cartToFrac)
+  
+        matrixList.push(m)
+      })
+  
+      return matrixList
+    }
+  
+    const unitcellAssembly = new Assembly('UNITCELL')
+    const unitcellMatrixList = getMatrixList()
+    const ncsMatrixList: Matrix4[] = []
+    if (structure.biomolDict.NCS) {
+      ncsMatrixList.push(
+        new Matrix4(), ...structure.biomolDict.NCS.partList[ 0 ].matrixList
+      )
+      const ncsUnitcellMatrixList: Matrix4[] = []
+      unitcellMatrixList.forEach(sm => {
+        ncsMatrixList.forEach(nm => {
+          ncsUnitcellMatrixList.push(sm.clone().multiply(nm))
+        })
+      })
+      unitcellAssembly.addPart(ncsUnitcellMatrixList)
+    } else {
+      unitcellAssembly.addPart(unitcellMatrixList)
+    }
+  
+    const vec = new Vector3()
+    const supercellAssembly = new Assembly('SUPERCELL')
+    const supercellMatrixList = Array.prototype.concat.call(
+      getMatrixList(vec.set(1, 0, 0)),  // 655
+      getMatrixList(vec.set(0, 1, 0)),  // 565
+      getMatrixList(vec.set(0, 0, 1)),  // 556
+  
+      getMatrixList(vec.set(-1, 0, 0)),  // 455
+      getMatrixList(vec.set(0, -1, 0)),  // 545
+      getMatrixList(vec.set(0, 0, -1)),  // 554
+  
+      getMatrixList(vec.set(1, 1, 0)),  // 665
+      getMatrixList(vec.set(1, 0, 1)),  // 656
+      getMatrixList(vec.set(0, 1, 1)),  // 566
+  
+      getMatrixList(vec.set(-1, -1, 0)),  // 445
+      getMatrixList(vec.set(-1, 0, -1)),  // 454
+      getMatrixList(vec.set(0, -1, -1)),  // 544
+  
+      getMatrixList(vec.set(1, -1, -1)),  // 644
+      getMatrixList(vec.set(1, 1, -1)),  // 664
+      getMatrixList(vec.set(1, -1, 1)),  // 646
+      getMatrixList(vec.set(-1, 1, 1)),  // 466
+      getMatrixList(vec.set(-1, -1, 1)),  // 446
+      getMatrixList(vec.set(-1, 1, -1)),  // 464
+  
+      getMatrixList(vec.set(0, 1, -1)),  // 564
+      getMatrixList(vec.set(0, -1, 1)),  // 546
+      getMatrixList(vec.set(1, 0, -1)),  // 654
+      getMatrixList(vec.set(-1, 0, 1)),  // 456
+      getMatrixList(vec.set(1, -1, 0)),  // 645
+      getMatrixList(vec.set(-1, 1, 0)),  // 465
+  
+      getMatrixList(),  // 555
+      getMatrixList(vec.set(1, 1, 1)),  // 666
+      getMatrixList(vec.set(-1, -1, -1))   // 444
+    )
+    if (structure.biomolDict.NCS) {
+      const ncsSupercellMatrixList: Matrix4[] = []
+      supercellMatrixList.forEach(function (sm: Matrix4) {
+        ncsMatrixList.forEach(function (nm) {
+          ncsSupercellMatrixList.push(sm.clone().multiply(nm))
+        })
+      })
+      supercellAssembly.addPart(ncsSupercellMatrixList)
+    } else {
+      supercellAssembly.addPart(supercellMatrixList)
+    }
+  
+    structure.biomolDict.UNITCELL = unitcellAssembly
+    structure.biomolDict.SUPERCELL = supercellAssembly
+  
+    if (Debug) Log.timeEnd('buildUnitcellAssembly')
   }
 }
 
