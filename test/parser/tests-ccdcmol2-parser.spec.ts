fa76309268d4d2c306dc25668d7c987bbcd45737
Andy Widdess <awiddess@ccdc.cam.ac.uk>
2022-01-14 15:25:32 +0000

    VIS-64-a added checks on unit cell to unit test

--- test/parser/tests-ccdcmol2-parser.spec.ts
+++ test/parser/tests-ccdcmol2-parser.spec.ts
@@ -16,6 +16,13 @@ describe('parser/ccdcmol2-parser', function () {
       return mol2Parser.parse().then(function (structure) {
         expect(structure.atomCount).toBe(37)
         expect(structure.bondCount).toBe(37)
+        expect(structure.unitcell.a).toBe(12.1359);
+        expect(structure.unitcell.b).toBe(9.8351);
+        expect(structure.unitcell.c).toBe(11.185);
+        expect(structure.unitcell.alpha).toBe(90);
+        expect(structure.unitcell.beta).toBe(90);
+        expect(structure.unitcell.gamma).toBe(90);
+        expect(structure.unitcell.volume).toBe(1335.016706350495);
       })
     })
   })
